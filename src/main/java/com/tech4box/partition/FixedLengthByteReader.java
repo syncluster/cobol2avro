ca8fd81639edb85ea44830490eca3f3de6cef899
Eduardo <eduardo@syncluster.com>
2025-06-01 16:47:44 -0300

    [1.2.0] PartitionUtils

--- src/main/java/com/tech4box/partition/FixedLengthByteReader.java
+++ src/main/java/com/tech4box/partition/FixedLengthByteReader.java
@@ -1,4 +1,73 @@
 package com.tech4box.partition;
 
-public class FixedLengthByteReader {
+import com.tech4box.JRecord.Details.AbstractLine;
+import com.tech4box.JRecord.Details.LayoutDetail;
+import com.tech4box.JRecord.Details.Line;
+import com.tech4box.JRecord.IO.AbstractLineReader;
+
+
+import java.io.*;
+
+public class FixedLengthByteReader extends AbstractLineReader {
+
+    private InputStream inputStream;
+    private LayoutDetail layout;
+    private final long startByte;
+    private final long endByte;
+    private long currentByte = 0;
+    private int recordLength;
+
+    public FixedLengthByteReader(long startByte, long endByte) {
+        this.startByte = startByte;
+        this.endByte = endByte;
+        this.recordLength = -1; // Will be initialized in open
+
+    }
+
+    @Override
+    public void open(InputStream inputStream, LayoutDetail layout) throws IOException {
+        this.inputStream = inputStream;
+        this.layout = layout;
+        this.recordLength = layout.getMaximumRecordLength();
+
+
+// Skip to start byte
+        long skipped = inputStream.skip(startByte);
+        if (skipped < startByte) {
+            throw new IOException("Failed to skip to start byte: " + startByte);
+        }
+        currentByte = startByte;
+    }
+
+    @Override
+    public AbstractLine readImplementation() throws IOException {
+        if (currentByte >= endByte) {
+            return null; // Reached assigned range end
+        }
+
+        byte[] buffer = new byte[recordLength];
+        int bytesRead = inputStream.read(buffer);
+
+        if (bytesRead == -1) {
+            return null;
+        }
+
+        currentByte += bytesRead;
+
+        if (bytesRead != recordLength) {
+            throw new IOException("Incomplete record read, expected " + recordLength + " but got " + bytesRead);
+        }
+
+        AbstractLine line = new Line(layout);
+        line.replace(buffer, 0, buffer.length);
+
+        return line;
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (inputStream != null) {
+            inputStream.close();
+        }
+    }
 }
--- src/main/java/com/tech4box/partition/PartitionUtils.java
+++ src/main/java/com/tech4box/partition/PartitionUtils.java
@@ -1,4 +1,16 @@
 package com.tech4box.partition;
 
 public class PartitionUtils {
+    public static long[] calculatePartition(long totalFileSize, int totalPartitions, int podIndex) {
+        long partitionSize = totalFileSize / totalPartitions;
+        long start = podIndex * partitionSize;
+
+// Last pod takes any remaining bytes
+        if (podIndex == totalPartitions - 1) {
+            long end = totalFileSize;
+            return new long[]{start, end - start};
+        } else {
+            return new long[]{start, partitionSize};
+        }
+    }
 }
